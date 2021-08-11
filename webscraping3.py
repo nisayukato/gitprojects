4d28460c773917fc21b82e6288b280ea0f080545
nisayukato <65362773+nisayukato@users.noreply.github.com>
2021-08-11 19:45:23 +0100

    instafollowersbot.py
    a program that i am still working on , it's a selenium bot that unfollow celebrities accounts then follow them

---
+++ webscraping3.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+import time
+browser = webdriver.Chrome()
+browser.get('https://www.instagram.com/')
+time.sleep(10)
+#signing in
+browser.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input').send_keys('')
+browser.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input').send_keys('')
+time.sleep(3)
+browser.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]').click()
+time.sleep(10)
+#passing the insta messages
+browser.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button').click()
+time.sleep(10)
+browser.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()
+time .sleep(3)
+#the list of the celebrities users you want
+users = ['kimkardashian','leomessi','cristiano','psg','fcbarcelona','neymarjr']
+#unfollowig the users
+for user in users :
+    try :
+        browser.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input').send_keys(user)
+        time.sleep(4.5)
+        browser.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div').click()
+        time.sleep(20)
+    except Exception :
+        print('there is a problem in searching for '+ user)
+    try :
+        browser.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/div[1]/div[2]/div/div[2]/div/span/span[1]/button/div/span').click()
+        time.sleep(2)
+        browser.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[1]').click()
+        time.sleep(2)
+    except Exception :
+        print('could not unfollow {}'.format(user))
+#following the users
+for account in users :
+    try :
+        time.sleep(20)
+        browser.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/div[1]/div[2]/div/div/div/span/span[1]/button').click()
+        browser.back()
+    except Exception :
+        print("couldn't follow {}".format(account))
+        browser.back()
+
